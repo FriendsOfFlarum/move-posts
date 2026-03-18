fa9a9be448a8950ce7fd3d56e13c380bf0007d66
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-03-18 18:11:22 +0100

    fix: prevent moderation controls outside discussion route

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -81,7 +81,7 @@ app.initializers.add('fof/move-posts', () => {
   });
 
   extend(PostControls, 'moderationControls', function (items, post) {
-    if (post.contentType() !== 'comment') return;
+    if (post.contentType() !== 'comment' || app.current.get('routeName') !== 'discussion') return;
 
     const postId = post.id();
     if (!postId) return;
